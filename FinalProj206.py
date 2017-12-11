e998f54f2f242a6dc81edb00eb78f841f0526e8a
David Piper <David@Jamess-MacBook-Pro-4.local>
2017-12-10 23:56:18 -0500

    project updates

--- FinalProj206.py
+++ FinalProj206.py
@@ -4,8 +4,29 @@
 import unittest
 import itertools
 import collections
-import facebook_info # same deal as always...
+import facebook_info
 import json
 import sqlite3
+import requests
+import facebook
+
+fb_id     = facebook_info.access_token
+fb_secret = facebook_info.secret_token
+
+
+CACHE_FNAME = "FinalProj206.json"
+
+
+try:
+	cache_file = open(CACHE_FNAME, 'r') #attempts to open data from file
+	cache_data = cache_file.read() #turns data to string
+	cache_file.close() #closes file since we have string
+	CACHE_DICTION = json.loads(cache_data) #puts data in dictionary
+except:
+	CACHE_DICTION = {}
+
+fb_site = "https://graph.facebook.com/v2.8/" + fb_secret + "/comments"
+site_params = {}
+site_params["access_token"]= fb_id
 
 
